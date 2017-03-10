75863589faabba80b39d6033f355aa7a4c9eeb6b
pcannon2012 <pcannon2012@gmail.com>
2017-03-09 19:28:49 -0500

    Checkpoint 8 attempt1

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,19 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+ var pointsArray = document.getElementsByClassName('point');
  
-                 var revealPoint = function(index) {
-                     points[index].style.opacity = 1;
-                     points[index].style.transform = "scaleX(1) translateY(0)";
-                     points[index].style.msTransform = "scaleX(1) translateY(0)";
-                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+ var animatePoints = function(points) {
                  };
+     window.onload = function() {
+              if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+        var sellingPoints = document.getElementsByClassName('selling-points')[0];
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      window.addEventListener('scroll', function(event) {
+           if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
 
                 for (var i = 0; i < points.length; i++) {
                     revealPoint(i);
