142de71a15ce67072b4e9b7b2ab838e4c2d5fe1f
pcannon2012 <pcannon2012@gmail.com>
2017-04-16 17:10:42 -0400

    Checkpoint 16 attempt 1

--- scripts/landing.js
+++ scripts/landing.js
@@ -14,13 +14,33 @@
     }
 };
 
- window.onload = function() {
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+ $(window).load(function() {
+     //1
+     if ($(window).height() > 950) {
+         animatePoints();
      }
-     window.addEventListener('scroll', function(event) {
-         if ( pointsArray[0].getBoundingClientRect().top <= 500 ) {
-             animatePoints(pointsArray);
+     //2
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     
+     //3
+     $(window).scroll(function(event) {
+         
+     // #4
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }
+     //#5
+     var animatePoints = function() {
+         var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
+         
+    //6
+         $.each($('.point'), revealPoint);
+     };
+ });
