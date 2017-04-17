3097f4e370e178152ab3c41a1458e1016e5cce5c
pcannon2012 <pcannon2012@gmail.com>
2017-04-17 14:56:20 -0400

    Checkpoint17 Problem with last 2 steps

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,7 @@
 var collectionItemTemplate =
+//#1
+var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,14 +16,25 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+    
+    //#2
+    retun $(template);
+};
+
  window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
+ $(window).load(funtion() {
+     
+    // #3
+    var $collectionContainer = $('.album-covers');
+     
+    // #4
+    $collectionContainer.empty();
  
-     // #3
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+    for (var i = 0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate();
+       
+        // #5
+        $collectionContainer.append($newThumbnail);
      }
- }
+}
+});
