1cfd2b4e42c245643145e1f0d82aeee26767d265
pcannon2012 <pcannon2012@gmail.com>
2017-06-24 08:04:08 -0400

    Checkpoint 19 2nd

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ }; 
+
+var albumBlackLab = {
+     title: 'Your Body Above Me',
+     artist: 'Paul Durham',
+     label: 'EM',
+     year: '1999',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Wash It Away', duration: '1:01' },
+         { title: 'She Loves Me', duration: '5:01' },
+         { title: 'X-Ray', duration: '3:21'},
+         { title: 'Time Ago', duration: '3:14' },
+         { title: 'Cant Keep the Rain', duration: '2:15'}
+     ]
+ };
